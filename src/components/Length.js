7c9eec2bf10d3140d198913a4acad4b38a2d38da
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-03-02 14:07:47 +0530

    few bug fixed

--- src/components/Length.js
+++ src/components/Length.js
@@ -1,29 +1,10 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Form from "./Form";
 import { Typography } from "@material-ui/core";
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
 export default function Length() {
-  const classes = useStyles();
   const unitData = [
     {
       value: "mm",
@@ -47,7 +28,7 @@ export default function Length() {
     },
   ];
   return (
-    <Card className={classes.root}>
+    <Card>
       <CardContent>
         <Typography>Length</Typography>
         <Form title="Length" unitData={unitData} />
